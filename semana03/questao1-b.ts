e4bec57d9f69186976e3fcd4b6af77b32e23d6f9
Aser Junior <61999214+aserjunior@users.noreply.github.com>
2021-09-13 22:29:43 -0300

    Add files via upload

---
+++ semana03/questao1-b.ts
@@ -0,0 +1,7 @@
+let b: any = 10;
+b = 2;
+
+/* Essa alternativa compila, pois
+ao adicionar o any essa variavel
+passa a aceitar a atribuição do 2 */
+
---
+++ semana03/questao1-c.ts
@@ -0,0 +1,8 @@
+let c: number = 10;
+    c = 2;
+
+
+/* Essa alternativa compila, pois ao
+colocar o number a variavel passa a
+aceitar numeros a sua atribuição */
+
